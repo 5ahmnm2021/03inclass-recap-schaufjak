9c82680edc6baadc082cec57c2fb7b6948a270c4
schaufjak <jakob.schauflinger@htl-salzburg.ac.at>
2021-03-19 16:42:32 +0100

    Finished Project

--- Assets/MyGame/Scipts/Controller.cs
+++ Assets/MyGame/Scipts/Controller.cs
@@ -1,48 +1,59 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
+
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Controller : MonoBehaviour
-{ 
-    
+{
+        private int counter = 0;
+        public InputField zahl1;
+        public InputField zahl2;
+        public Text ergebnis;
+        public Image farbfeld;
 
-        public InputField Zahl1;
-        public InputField Zahl2;
-        public Text Ergebnis;
-
-        private float Zahl1Float;
-        private float Zahl2Float;
-
-
-    public void Addition()
-    {
+        private float zahl1Float;
+        private float zahl2Float;
         
 
 
-        try
-        {
-            Zahl1Float = float.Parse(Zahl1.text);
-            Zahl1.image.color = new Color32(255, 255, 255, 255);
-        }
-        catch (System.Exception)
-        {
-            Zahl1.image.color = new Color32(255, 0, 0, 255);
-            Zahl1.text = "Zahl versuchen!";
-        }
-        try
-        {
-            Zahl2Float = float.Parse(Zahl2.text);
-            Zahl2.image.color = new Color32(255, 255, 255, 255);
-        }
-        catch (System.Exception)
-        {
-            Zahl2.image.color = new Color32(255, 0, 0, 255);
-            Zahl2.text = "Zahl versuchen!";
-        }
-        Ergebnis.text = (Zahl1Float + Zahl2Float).ToString();
+    void Start()
+    {
+        farbfeld.color = new Color32(102, 47, 84, 100);
+    }
 
+    void Update()
+    {
+
+        if (Input.GetKeyDown("space"))
+        {
+            if (counter == 0)
+            {
+                farbfeld.color = new Color32(255, 205, 25, 100);
+            }
+
+            if (counter == 1)
+            {
+                farbfeld.color = new Color32(95, 34, 0, 100);
+            }
+
+            if (counter == 2)
+            {
+                farbfeld.color = new Color32(207, 63, 21, 100);
+            }
+
+            if (counter == 3)
+            {
+                farbfeld.color = new Color32(102, 47, 84, 100);
+            }
+
+            counter++;
+
+            if (counter == 4)
+            {
+                counter = 0;
+            }
+        }
     }
 
 
@@ -62,5 +73,32 @@ public class Controller : MonoBehaviour
     {
         SceneManager.LoadScene("00WelcomeScene");
        
+    }
+
+    public void Addition()
+    {
+        try
+        {
+            zahl1Float = float.Parse(zahl1.text);
+            zahl1.image.color = new Color32(255, 255, 255, 255);
+        }
+        catch (System.Exception)
+        {
+            zahl1.image.color = new Color32(255, 0, 0, 255);
+            zahl1.text = "Zahl versuchen!";
+            
+        }
+        try
+        {
+            zahl2Float = float.Parse(zahl2.text);
+            zahl2.image.color = new Color32(255, 255, 255, 255);
+        }
+        catch (System.Exception)
+        {
+            zahl2.image.color = new Color32(255, 0, 0, 255);
+            zahl2.text = "Zahl versuchen!";
+            
+        }
+        ergebnis.text = (zahl1Float + zahl2Float).ToString();
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # 03inclass-recap-schaufjak
+
+Project description:
+Erstellen eines 3-Seitigen Rumspring-Projekts
+
+Development platform:
+(Windows, Unity 2020.1.5, Visual Studio 16.8.5, used SDKs)
+
+Target platform:
+WebGL 960X600
+
+Visuals:
+Screenshots (concept and experience), Video
+
+Necessary setup/execution steps:
+Installation process, e.g. step by step instructions that I can run the project after cloning it
+
+Third party material:
+(if used Fonts, Sounds, Music, Graphics, Materials, Code etc.)
+
+Project state:
+Interface, finished Script (100%)
+
+Limitations:
+none
+
+Lessons Learned:
+Copyright by Jakob Schauflinger 
